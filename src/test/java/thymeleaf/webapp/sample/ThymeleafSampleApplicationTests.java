3241ac2e48d9ee402372bb54ad7f0c9a39851ea7
Denis Berretti <denis.berretti95@gmail.com>
2025-09-03 19:37:01 +0200

    first commit

---
+++ src/test/java/thymeleaf/webapp/sample/ThymeleafSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package thymeleaf.webapp.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
